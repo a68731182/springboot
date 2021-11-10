deadc8efd18e211be70511f1cb9261de339f8b8b
wangzengqing <925551747@qq.com>
2021-11-10 10:51:09 +0800

    测试提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("com.example.demo.mapper")// 该注解用于将相对应的mapper下的接口生成实体类   相比于@Scan 不用挨个在每个Uservice上加一个@Scan注解
+@MapperScan("com.example.demo.mapper")//该注解用于将相对应的mapper下的接口生成实体类   相比于@Scan 不用挨个在每个Uservice上加一个@Scan注解
 @SpringBootApplication
 public class DemoApplication {
 
